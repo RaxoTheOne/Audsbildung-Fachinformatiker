8852be069910e2d351a91f70bf126d9015bb41a2
RaxoTheOne <traxon.bg@gmail.com>
2025-05-03 17:17:24 +0200

    changed name

---
+++ Dienstag:29.04.2025/Ueberraschungseier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int kinder;
+    int eier;
+    int eierProKind;
+    int rest;
+
+    printf("Anzahl der Kinder: ");
+    scanf("%d", &kinder);
+
+    printf("Anzahl der Überraschungseier");
+}
