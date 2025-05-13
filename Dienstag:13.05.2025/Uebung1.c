f59de5129a592690e0b177595c4a8f7cf74635c0
RaxoTheOne <traxon.bg@gmail.com>
2025-05-13 10:35:04 +0200

    `Added new function ausgabe2 and called it in main, updated ausgabe function to match new function signature`

--- Dienstag:13.05.2025/Uebung1.c
+++ Dienstag:13.05.2025/Uebung1.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 void ausgabe(int *, int);
+void ausgabe2(int *, int);
 
 int main(){
     int arr[] = {1, 2, 3, 4, 5};
 
     ausgabe(arr, sizeof(arr) / sizeof(int));
+    ausgabe2(arr, sizeof(arr) / sizeof(int));
 
     return 0;
 }
@@ -15,5 +17,11 @@ void ausgabe(int * ipoi, int anz){
     for(int i = 0; i < anz; i++){
         printf("%s", ipoi[i] % 2 ? "un" : "");
         printf("%s %d\n", "gerade", ipoi[i]);
+    }
+}
+
+void ausgabe2(int * ipoi, int anz){
+    for(int *poi = ipoi; poi < ipoi + anz; poi++){
+        printf("%d %s%s\n", *poi, *poi % 2 ? "un" : "", "gerade");   
     }
 }
